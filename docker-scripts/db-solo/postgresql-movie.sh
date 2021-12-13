6cef1d991d06a95047b9686f0dd1300e33a5c946
matthcol <matthias@learning64.net>
2021-12-13 15:17:26 +0100

    docker with volumes

--- docker-scripts/db-solo/postgresql-movie.sh
+++ docker-scripts/db-solo/postgresql-movie.sh
@@ -5,6 +5,7 @@ docker run --detach \
 	--env POSTGRES_DB=dbmovie \
 	--env POSTGRES_USER=movie \
 	--env POSTGRES_PASSWORD=password \
+	-v vpgdbmovie:/var/lib/postgresql/data \
 	docker.io/library/postgres:latest
 
 
